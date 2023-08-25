019c76aafcc0541d2394d48093767729241dc5a9
Karmukilan Prakash <115093024+karmukilan-12@users.noreply.github.com>
2023-08-25 22:05:29 +0530

    Update competitions.js

--- src/data/competitions.js
+++ src/data/competitions.js
@@ -3,10 +3,11 @@ export const competitions = [
         imageurl:`https://static.vecteezy.com/system/resources/previews/023/856/960/large_2x/summer-seasonal-anime-style-background-landscape-mountain-lake-and-relax-vibes-anime-background-or-wallpaper-generative-ai-free-photo.jpg`,
     competition:
     [
-        {val:'Artist Alley',url:'https://forms.gle/cCAwkiuJAk8JwkeE9'}
-        ,{val:'Anime Quiz',url:'https://forms.gle/KR61iMByc5EGCxAZA'},
+        {val:'Artist Alley',url:'https://forms.gle/cCAwkiuJAk8JwkeE9'},
         {val:'Volley Ball',url:'https://forms.gle/niVwUo67yjAUPr8MA'},
-        {val:'Cosplay',url:'https://forms.gle/YCPJk9h6GxBQnb3g8'}
+        {val:'Cosplay Competition',url:'https://forms.gle/YCPJk9h6GxBQnb3g8'},
+        {val:'Cosplay Workshop',url:'https://forms.gle/jm5z5ezQPwKPTrME6'},
+
 
     ]
 
