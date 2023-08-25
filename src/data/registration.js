b936a8f1b6af877f414dbcd734b45e3adf244ab9
Karmukilan Prakash <115093024+karmukilan-12@users.noreply.github.com>
2023-08-25 12:25:38 +0530

    Update registration.js

--- src/data/registration.js
+++ src/data/registration.js
@@ -2,30 +2,24 @@ export const registration = [
     {
       imageurl: 'https://static.vecteezy.com/system/resources/previews/023/856/960/large_2x/summer-seasonal-anime-style-background-landscape-mountain-lake-and-relax-vibes-anime-background-or-wallpaper-generative-ai-free-photo.jpg',
       day: 'Day 1',
-      basicplan: '44',
-      premiumplan: '99',
-      url:'https://forms.gle/KR61iMByc5EGCxAZA'
-    },
-    {
-      imageurl: 'https://static.vecteezy.com/system/resources/previews/023/856/960/large_2x/summer-seasonal-anime-style-background-landscape-mountain-lake-and-relax-vibes-anime-background-or-wallpaper-generative-ai-free-photo.jpg',
-      day: 'Day 1',
-      basicplan: '44',
-      premiumplan: '99',
+      basicplan: 'Rs.44',
+      premiumplan: 'Rs.99',
       url:'https://forms.gle/KR61iMByc5EGCxAZA'
     },
     {
       imageurl: 'https://static.vecteezy.com/system/resources/previews/023/856/960/large_2x/summer-seasonal-anime-style-background-landscape-mountain-lake-and-relax-vibes-anime-background-or-wallpaper-generative-ai-free-photo.jpg',
       day: 'Day 2',
-      basicplan: '44',
-      premiumplan: '99',
+      basicplan: 'Rs.49',
+      premiumplan: 'Rs.99',
       url:'https://forms.gle/KR61iMByc5EGCxAZA'
     },
     {
       imageurl: 'https://static.vecteezy.com/system/resources/previews/023/856/960/large_2x/summer-seasonal-anime-style-background-landscape-mountain-lake-and-relax-vibes-anime-background-or-wallpaper-generative-ai-free-photo.jpg',
-      day: 'Day 1',
-      basicplan: '44',
-      premiumplan: '99',
+      day: 'Both Days - Combo',
+      basicplan: '--',
+      premiumplan: 'Rs.120',
       url:'https://forms.gle/KR61iMByc5EGCxAZA'
     },
+   
 
   ];
