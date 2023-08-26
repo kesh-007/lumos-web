3020248ddccf138f76143fc96fb06ac2213193be
data-overflow <kvrjr27@gmail.com>
2023-08-26 23:31:50 +0530

    Update ExhibitCard.jsx

--- src/Components/ExhibitCard.jsx
+++ src/Components/ExhibitCard.jsx
@@ -5,11 +5,11 @@ import '../App.css';
 const ExhibitCard = ({ exhibits }) => {
 
   return (
-    <div className='grid grid-cols-1 gap-5  max-md:flex xl:grid-cols-2 max-md:flex-col '>
+    <div className='grid grid-cols-1 gap-5  max-md:flex 2xl:grid-cols-2 max-md:flex-col '>
       {exhibits.map((exhibit) => (
-        <div className='flex mx-1x` mb-3 bg-[#2A2929]  max-md:flex-col' key={exhibit.id}>
-          <div className='w-[50%] max-md:w-full'>
-            <img src={exhibit.imageurl} alt='image' className='h-full object-cover' loading='lazy' />
+        <div className='flex mx-1x` mb-3 bg-[#2A2929]  max-md:flex-col justify-center' key={exhibit.id}>
+          <div className='md:w-[50%] max-md:w-full'>
+            <img src={exhibit.imageurl} alt='exhibits' className='h-full object-cover' loading='lazy' />
           </div>
           <div className='w-full'>
             <div className='bg-[#FF0000] text-4xl p-4 font-bold'>
