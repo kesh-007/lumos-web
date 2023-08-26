2ecafd9e7c6de3aff64188b9fc6f3fbdfc9f4423
data-overflow <kvrjr27@gmail.com>
2023-08-26 12:05:37 +0530

    added all posters
    temporary posters

--- src/data/exhibits.js
+++ src/data/exhibits.js
@@ -1,27 +1,27 @@
 export const exhibits = [
     {
-        name:"Miniature Garden Exhibit",
-        descp:"Enter an exquisite Japanese-themed miniature garden—a realm of Zen, with miniature trees that represent harmony and balance in nature. Discover nature's splendor in delicate structures and resplendent flora. Immerse in Japan's cultural heritage, where artistry and tranquility intertwine. Let the whispers of the wind embrace your soul, weaving a tapestry of peace ",
-        imageurl:`https://www.cnet.com/a/img/resize/14a47132fd55bf90351277b0a8f9fd231287eeb6/hub/2023/07/05/b8503974-3baa-4311-bfa1-5e5b747c83ad/jujutsu-kaisen-season-2.jpg?auto=webp&fit=crop&height=1200&width=1200`
+        name: "Miniature Garden Exhibit",
+        descp: "Enter an exquisite Japanese-themed miniature garden—a realm of Zen, with miniature trees that represent harmony and balance in nature. Discover nature's splendor in delicate structures and resplendent flora. Immerse in Japan's cultural heritage, where artistry and tranquility intertwine. Let the whispers of the wind embrace your soul, weaving a tapestry of peace ",
+        imageurl: process.env.PUBLIC_URL + "/img/garden.png",
     },
     {
-        name:"JAPANESE GAMES",
-        descp:"Delight in traditional Japanese non-digital games, a charming glimpse into their culture. Games that are loved and adored by the Japanese, would be on display for participants to try and have fun with. A chance to let Indians know about Japanese culture and how they spend time with their loved ones, cherishing joyful moments and bonding over these cherished pastimes. A cultural exchange that bridges hearts, bringing Japan's essence to India's doorstep.",
-        imageurl:`https://www.cnet.com/a/img/resize/14a47132fd55bf90351277b0a8f9fd231287eeb6/hub/2023/07/05/b8503974-3baa-4311-bfa1-5e5b747c83ad/jujutsu-kaisen-season-2.jpg?auto=webp&fit=crop&height=1200&width=1200`
+        name: "JAPANESE GAMES",
+        descp: "Delight in traditional Japanese non-digital games, a charming glimpse into their culture. Games that are loved and adored by the Japanese, would be on display for participants to try and have fun with. A chance to let Indians know about Japanese culture and how they spend time with their loved ones, cherishing joyful moments and bonding over these cherished pastimes. A cultural exchange that bridges hearts, bringing Japan's essence to India's doorstep.",
+        imageurl: process.env.PUBLIC_URL + "/img/games.png",
     },
     {
-        name:"ACTION FIGURINES (JAPANESE DOLLS AND CHARACTERS)",
-        descp:"Embark on a captivating journey through the Action-Figurines exhibit—a celebration of Japanese artistry and intricate traditional dolls. Marvel at beloved anime, manga, and folklore characters brought to life in stunning detail. Witness the interplay of color and emotion, as these figurines weave tales of valor and ancient traditions. Immerse yourself in a world of imagination and cultural appreciation, connecting with Japan's artistic soul—a must-see experience for artists, art lovers, and anime fans alike.",
-        imageurl:`https://www.cnet.com/a/img/resize/14a47132fd55bf90351277b0a8f9fd231287eeb6/hub/2023/07/05/b8503974-3baa-4311-bfa1-5e5b747c83ad/jujutsu-kaisen-season-2.jpg?auto=webp&fit=crop&height=1200&width=1200`
+        name: "ACTION FIGURINES (JAPANESE DOLLS AND CHARACTERS)",
+        descp: "Embark on a captivating journey through the Action-Figurines exhibit—a celebration of Japanese artistry and intricate traditional dolls. Marvel at beloved anime, manga, and folklore characters brought to life in stunning detail. Witness the interplay of color and emotion, as these figurines weave tales of valor and ancient traditions. Immerse yourself in a world of imagination and cultural appreciation, connecting with Japan's artistic soul—a must-see experience for artists, art lovers, and anime fans alike.",
+        imageurl: process.env.PUBLIC_URL + "/img/16.png",
     },
     {
-        name:"PHOTO BOOTH",
-        descp:"Step into a whimsical booth with Japanese and anime-themed props for delightful photoshoots with friends. Freeze moments of laughter and camaraderie in enchanting costumes. Unleash your inner hero, embracing the charm of Japanese pop culture. Capture unforgettable snapshots of fantastical adventures, cherishing memories in photographic fragments. Share the joy and immerse yourself in the vibrant world of imagination.",
-        imageurl:`https://www.cnet.com/a/img/resize/14a47132fd55bf90351277b0a8f9fd231287eeb6/hub/2023/07/05/b8503974-3baa-4311-bfa1-5e5b747c83ad/jujutsu-kaisen-season-2.jpg?auto=webp&fit=crop&height=1200&width=1200`
+        name: "PHOTO BOOTH",
+        descp: "Step into a whimsical booth with Japanese and anime-themed props for delightful photoshoots with friends. Freeze moments of laughter and camaraderie in enchanting costumes. Unleash your inner hero, embracing the charm of Japanese pop culture. Capture unforgettable snapshots of fantastical adventures, cherishing memories in photographic fragments. Share the joy and immerse yourself in the vibrant world of imagination.",
+        imageurl: process.env.PUBLIC_URL + "/img/18.png",
     },
     {
-        name:"Cultural Exhibit (Traditions of Japan )",
-        descp:"From Japanese festival games and customs to food habits and clothing. This exhibit would let students learn in-depth about Japanese culture, their habits, clothing, recreation, etc. as they come into close contact with Japan’s culture. It might include kanji drawing, kimono wearing, chopstick tryout, tea culture, fortune picking etc.        ",
-        imageurl:`https://www.cnet.com/a/img/resize/14a47132fd55bf90351277b0a8f9fd231287eeb6/hub/2023/07/05/b8503974-3baa-4311-bfa1-5e5b747c83ad/jujutsu-kaisen-season-2.jpg?auto=webp&fit=crop&height=1200&width=1200`
+        name: "Cultural Exhibit (Traditions of Japan )",
+        descp: "From Japanese festival games and customs to food habits and clothing. This exhibit would let students learn in-depth about Japanese culture, their habits, clothing, recreation, etc. as they come into close contact with Japan’s culture. It might include kanji drawing, kimono wearing, chopstick tryout, tea culture, fortune picking etc.        ",
+        imageurl: process.env.PUBLIC_URL + "/img/17.png",
     },
 ]
