d1165b57eced45888ec9a488620835deb41ddb1a
Karmukilan Prakash <115093024+karmukilan-12@users.noreply.github.com>
2023-08-26 14:20:52 +0530

    Update registration.js

--- src/data/registration.js
+++ src/data/registration.js
@@ -2,7 +2,7 @@ export const registration = [
     {
       imageurl: 'https://static.vecteezy.com/system/resources/previews/023/856/960/large_2x/summer-seasonal-anime-style-background-landscape-mountain-lake-and-relax-vibes-anime-background-or-wallpaper-generative-ai-free-photo.jpg',
       day: 'Day 1',
-      basicplan: 'Rs.44',
+      basicplan: 'Rs.49',
       premiumplan: 'Rs.99',
       url:'https://forms.gle/KR61iMByc5EGCxAZA'
     },
